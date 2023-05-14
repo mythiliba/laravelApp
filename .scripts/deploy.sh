956bcd050eb171639effc3b947e1e00379b58036
mythiliba <mythilibala1985@gmail.com>
2023-05-14 20:52:02 +0530

    Create deploy.sh

---
+++ .scripts/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+cd laravelApp
+(php artisan down) || true
+
+# Pull the latest version of the app
+git pull origin master
+
+# Install composer dependencies
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Clearing Cache
+php artisan cache:clear
+php artisan config:clear
+
+# Recreate cache
+php artisan optimize
+
+# Run database migrations
+php artisan migrate --force
+
+# Reload PHP to update opcache
+echo "" | sudo -S service php reload
+
+# Exit maintenance mode
+php artisan up
+
+echo "Deployment finished!"
