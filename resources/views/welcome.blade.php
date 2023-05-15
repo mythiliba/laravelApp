cc9d2200298a850bbd613ad3355a31a27f0fe425
mythiliba <mythilibala1985@gmail.com>
2023-05-15 13:53:39 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <h1> Hello World .........!</h1>
+        <h1> laravel </h1>
     </body>
 </html>
