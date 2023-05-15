7643b42d6e35ec39f873a964588d90e26c95323d
mythiliba <mythilibala1985@gmail.com>
2023-05-15 20:10:16 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <h1> Helloworld  !!!!! </h1>
+        <h1> This is Laravel App ................ </h1>
     </body>
 </html>
